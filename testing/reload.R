39b888c6358a9b26d65fc0a032d6438a07e6172d
Umberto Vesco <umberto.vesco@gmail.com>
2023-03-15 00:14:47 +0100

    traduzione nome prima lezione

---
+++ testing/reload.R
@@ -0,0 +1,4 @@
+# script da eseguire dopo aver fatto pull per valutare la nuova versione in locale
+library(swirl)
+uninstall_course("R_Programming_E_IT")
+install_course_github("uvesco", "R_Programming_E_IT", multi=F)
